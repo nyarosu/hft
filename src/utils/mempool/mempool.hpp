f27bb7875247029f0e9fd771bde741331dfa0332
nyarosu <ronak.chatterjee65@gmail.com>
2023-08-13 20:57:37 +0100

    mempool

--- src/utils/mempool/mempool.hpp
+++ src/utils/mempool/mempool.hpp
@@ -4,6 +4,7 @@
  * @brief Dynamically allocated memory pool class
  * @version 0.1
  * @date 2023-08-13
+ * @test tests/utils/mempool_test.cpp
  * 
  * @copyright Copyright (c) 2023
  * 
@@ -19,7 +20,7 @@ namespace utils {
     /**
      * @brief Dynamically allocated memory pool. A large amount of memory is allocated up front, during construction.
      * Then, you can request chunks of memory from the pool, and this will be much faster as the memory has already been allocated.
-     * @tparam T 
+     * @tparam T Type of object held in pool, must have a default constructor
      */
     template<typename T>
     class MemPool final {
@@ -35,13 +36,14 @@ namespace utils {
 
     public:
         /**
-         * @brief Create a new memory pool.
-         * Pool is initialized with all elements default constructed and marked as free.
+         * @brief Create a new memory pool. Pool is initialized with all elements default constructed and marked as free.
          * @param size Number of elements (of type T) in pool. 
+         * @note T must have a default constructor
          */
-        explicit MemPool(size_t size)
+        explicit MemPool(int size)
             : m_data(size, { T(), true }) 
         {
+            // This assert is required to ensure safety of an efficiency trick used when deallocating - see MemPool::deallocate().
             ASSERT(reinterpret_cast<const Block*>(&(m_data[0].data)) == &(m_data[0]), "Alignment error; T is not first member of struct.");
         }
         
@@ -72,31 +74,44 @@ namespace utils {
          */
         template<typename... Args>
         T* allocate(Args... args) noexcept {
-            Block* ptr { &m_data[m_next_free_index] };
-            ASSERT(ptr->is_free, "Next free index is in use.");
-            
-            // Get a pointer to the object portion of the memory block.
+            Block* ptr { &(m_data[m_next_free_index]) };
+            ASSERT(ptr->is_free, "Pool is full.");
+        
             T* obj { &(ptr->data) };
 
             // Construct new T, forwarding arguments, directly inside obj
             new(obj) T(std::forward<Args>(args)...);
+            ptr->is_free = false;
 
             updateFreeIndex();
             return obj;
         }
 
-        void deallocate(const T* elem) noexcept {};
+        /**
+         * @brief Deallocate the element inside the pool given by the ptr elem.
+         * Checks that pointer points to a valid element inside the pool. If it points to something not in the pool, or to something that's already been deallocated,
+         * an assertion error will 
+         * @param elem Pointer to the element to be deallocated
+         */
+        void deallocate(const T* elem) noexcept {
+            auto index { reinterpret_cast<const Block*>(elem) - &m_data[0] };
+            
+            // If the given pointer doesn't point to an element within the pool, this index will be out of bounds
+            ASSERT(index >= 0 && index < m_data.size(), "Pointer provided doesn't point to something in this pool.");
+            
+            ASSERT(!m_data[index].is_free, "Attempted to deallocate unallocated entry in pool.");
+            m_data[index].is_free = true;
+        };
 
     private:
         /**
          * @brief Internal helper method called when pool changes to find
          * and set the next free index.
-         * The idea is to start from the initial index and walk forward, trying to find a free block.
+         * The idea is to start from the initial index and walk forward, trying to find a free block, since that's where we are most likely to find a free block.
          * If we don't find one, we start from 0 (maybe something was deallocated) and walk up to initial_free_index.
-         * If we still don't find one, the memory pool is full.
-         * @return void
+         * If we still don't find one, the memory pool is full and we log a fatal error.
          */
-        void updateFreeIndex() noexcept {
+        void updateFreeIndex() noexcept { 
             const auto initial_free_index { m_next_free_index };
             while (!m_data[m_next_free_index].is_free) {
                 ++m_next_free_index;
@@ -112,8 +127,5 @@ namespace utils {
                 }
             }
         }
-
-
     };
-
 }
--- tests/utils/mempool_test.cpp
+++ tests/utils/mempool_test.cpp
@@ -1,8 +1,52 @@
 #include "mempool/mempool.hpp"
 #include <gtest/gtest.h>
 
-TEST(MemPool, PoolIsInitialized)
-{
-    utils::MemPool<int> pool{10};
-    pool.allocate();
+using namespace utils;
+
+class TestObj {
+private:
+    int m_value {-1};
+public:
+    TestObj() = default;
+    explicit TestObj(int value)
+        : m_value { value }
+    {}
+    int getValue() { return m_value; }
+    void setValue(int value) { m_value = value; }
+};
+
+TEST(MemPoolTests, PoolSizeValidation) {
+    ASSERT_THROW(MemPool<TestObj>{ -10 }, std::exception);
 }
+
+TEST(MemPoolTests, CreateAndUseValidPool) {
+    // Create empty pool
+    MemPool pool { MemPool<TestObj>{ 1000 } };
+    
+    TestObj* new_obj { pool.allocate(5) };
+    
+    // Sanity check
+    ASSERT_EQ(new_obj->getValue(), 5);
+    new_obj->setValue(10);
+    ASSERT_EQ(new_obj->getValue(), 10);
+
+    // Allocate another object
+    TestObj* new_obj2 { pool.allocate(15) };
+    ASSERT_EQ(new_obj2->getValue(), 15);
+}
+
+TEST(MemPoolTests, AllocatingFullPoolFails) {
+    MemPool pool { MemPool<TestObj>{ 2 } };
+    pool.allocate(20);
+    pool.allocate(50);
+    
+    // Pool is full!
+    ASSERT_DEATH(pool.allocate(20), "Memory pool out of space.");
+}
+
+TEST(MemPoolTests, ResizePool) {}
+
+TEST(MemPoolTests, ValidDeallocation) {}
+
+TEST(MemPoolTests, InvalidDeallocation) {}
+
