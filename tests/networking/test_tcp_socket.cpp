901652799a33b69a8fb73a7d87a751aa4a1bf268
nyarosu <ronak.chatterjee65@gmail.com>
2023-09-03 16:34:55 +0100

    tcp

--- tests/networking/test_tcp_socket.cpp
+++ tests/networking/test_tcp_socket.cpp
@@ -7,3 +7,15 @@ TEST(CreateAndConnect, TCPSocketTests) {
     int fd { socket.connect("www.google.com", "enp3s0", 80, false) };
     ASSERT_NE(fd, -1);
 }
+
+TEST(CreateAndConnectAndSend, TCPSocketTests) {
+    logger::Logger logger { "test2" };
+    networking::TCPSocket socket { logger };
+    int fd { socket.connect("www.google.com", "enp3s0", 80, false) };
+    ASSERT_NE(fd, -1);
+    char data[] { "GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n" };
+    int len { socket.send(data, sizeof(data)) };
+    bool res { socket.sendAndRecv() };
+    ASSERT_TRUE(res);
+    ASSERT_EQ(len, sizeof(data));
+}
