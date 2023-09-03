8d4a83d976a78803548b7087957ea98b93e93d97
nyarosu <ronak.chatterjee65@gmail.com>
2023-09-03 22:59:00 +0100

    tcp

--- tests/networking/test_tcp_socket.cpp
+++ tests/networking/test_tcp_socket.cpp
@@ -1,13 +1,61 @@
 #include <gtest/gtest.h>
 #include "networking/tcp_socket.hpp"
+#include "test_utils/tcp_socket_fixture.hpp"
+#include <optional>
 
-TEST(CreateAndConnect, TCPSocketTests) {
-    logger::Logger logger { "test" };
-    networking::TCPSocket socket { logger };
-    int fd { socket.connect("www.google.com", "enp3s0", 80, false) };
+class TCPSocketTests : public TCPSocketFixture {};
+
+TEST_F(TCPSocketTests, CreateTCPSocket) {
+    int fd { socket_->connect("www.google.com", "enp3s0", 80, false) };
     ASSERT_NE(fd, -1);
 }
 
-TEST(CreateAndConnectAndSend, TCPSocketTests) {
+TEST_F(TCPSocketTests, ConnectToServer) {
+    int tempSock = socket(AF_INET, SOCK_STREAM, 0);
+sockaddr_in addr;
+addr.sin_family = AF_INET;
+addr.sin_port = htons(server_port_);
+inet_pton(AF_INET, server_addr_.c_str(), &addr.sin_addr);
 
+int result = connect(tempSock, (struct sockaddr*)&addr, sizeof(addr));
+if (result == 0) {
+    // Successfully connected, server is listening
+    close(tempSock);  // Important: Close the temporary socket after testing
+} else {
+    ASSERT_TRUE(false);
+}
+}
+
+TEST_F(TCPSocketTests, SendDataTCPSocket) {
+    // Connect to test server
+    int fd { socket_->connect(server_addr_, "lo", server_port_, false) };
+    ASSERT_NE(fd, -1);
+
+    // Accept connection on server
+    auto server_client_socket { acceptClient() };
+    ASSERT_NE(server_client_socket, nullptr);
+    
+    // Send some data
+    std::string data { "Hello World!" };
+    int ret { socket_->send(data.c_str(), data.size()) };
+    ASSERT_EQ(ret, data.size());
+
+    bool recieved { socket_->sendAndRecv() };
+
+    // We aren't expecting to receive any data
+    ASSERT_FALSE(recieved);
+
+    // Wait for server to receive data
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    // Check that it was received
+    char buf[1024];
+    auto read { readFromServer(*server_client_socket ,buf, data.size()) };
+
+    ASSERT_TRUE(read.has_value());
+    ASSERT_EQ(read.value(), data.size());
+
+    // Check that the data is correct
+    std::string read_data { buf };
+    ASSERT_EQ(read_data, data);
 }
--- tests/test_utils/include/test_utils/tcp_socket_fixture.hpp
+++ tests/test_utils/include/test_utils/tcp_socket_fixture.hpp
@@ -4,3 +4,57 @@
  * @brief Test fixture for testing tcp sockets
  * @copyright Copyright (c) 2023
  */
+
+#include <gtest/gtest.h>
+#include "networking/tcp_socket.hpp"
+#include "logging_fixture.hpp"
+
+
+class TCPSocketFixture: public LoggingFixture {
+protected:
+    /**
+     * @brief Set up sockets, loggers etc.
+     */
+    void SetUp() override;
+
+    /**
+     * @brief Destroy sockets, delete logger object/file, other cleanup
+     */
+    void TearDown() override;
+
+    /**
+     * @brief Ptr to TCP socket object to be used in tests
+     */
+    std::unique_ptr<networking::TCPSocket> socket_ { nullptr };
+
+    /**
+     * @brief Ptr to server socket object to be used in tests
+     */
+    std::unique_ptr<networking::TCPSocket> server_ { nullptr };
+
+    /**
+     * @brief Address of server socket
+     */
+    std::string server_addr_ {};
+
+    /**
+     * @brief Port of server socket
+     */
+    uint16_t server_port_ {};
+
+    /**
+     * @brief Server sockaddr struct, used for accepting/etc
+     */
+    sockaddr_in server_sockaddr_ {};
+
+    /**
+     * @brief Accept a connection from a client on the server socket
+     * @returns std::optional<networking::TCPSocket> client socket to communicate with client
+     */
+    std::unique_ptr<networking::TCPSocket> acceptClient();
+
+    /**
+     * @brief Read data from server
+     */
+    std::optional<int> readFromServer(networking::TCPSocket& server_client_socket, char* buf, size_t len);
+};
